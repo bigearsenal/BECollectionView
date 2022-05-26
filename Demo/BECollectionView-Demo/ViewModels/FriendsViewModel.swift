b6ce63047533f4d3c24f03e99624c13f159494e8
Chung Tran <bigearsenal@gmail.com>
2022-05-26 11:15:32 +0700

    feat: add demo project

---
+++ Demo/BECollectionView-Demo/ViewModels/FriendsViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  FriendsViewModel.swift
+//  BECollectionView_Example
+//
+//  Created by Chung Tran on 15/03/2021.
+//  Copyright © 2021 CocoaPods. All rights reserved.
+//
+
+import Foundation
+import BECollectionView
+import RxSwift
+
+class FriendsViewModel: BEListViewModel<Friend> {
+    override func createRequest() -> Single<[Friend]> {
+        Single<[Friend]>.just(data).delay(.seconds(Int.random(in: 2..<6)), scheduler: MainScheduler.instance)
+            .map { _ in
+                Array(
+                    [
+                        Friend(name: "Ty", numberOfLegs: 1),
+                        Friend(name: "Phi", numberOfLegs: 2),
+                        Friend(name: "Phid", numberOfLegs: 3)
+                    ].prefix(Bool.random() ? 0: 3)
+                )
+            }
+    }
+}
